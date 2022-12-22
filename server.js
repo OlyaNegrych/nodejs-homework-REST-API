44f491fb3dfda5b2f8211d7a137eb4732bb790bd
OlyaNegrych <olyanegrych@gmail.com>
2022-12-23 00:07:35 +0200

    added iife in server.js

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
 const app = require("./app");
 const { connectMongoose } = require("./db/connection");
 
-const start = async () => {
- try {
-   await connectMongoose();
-   console.log("Database connection successful");
- } catch (error) {
-   console.log(error.message);
-   process.exit(1);
- }
+(async () => {
+  try {
+    await connectMongoose();
+    console.log("Database connection successful");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
 
   app.listen(process.env.PORT, () => {
     console.log(`Server running. Use our API on port: ${process.env.PORT}`);
   });
-};
+})();
 
-start();
+// const start = async () => {
+//   try {
+//     await connectMongoose();
+//     console.log("Database connection successful");
+//   } catch (error) {
+//     console.log(error.message);
+//     process.exit(1);
+//   }
+
+//   app.listen(process.env.PORT, () => {
+//     console.log(`Server running. Use our API on port: ${process.env.PORT}`);
+//   });
+// };
+
+// start();
