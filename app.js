86523f8b291164f951b771ea3deae83ac6e1ba65
OlyaNegrych <olyanegrych@gmail.com>
2022-12-25 12:16:39 +0200

    added user model

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
+// require("./config/config-passport");
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-
 const contactsRouter = require("./routes/api/contacts");
-
 const app = express();
-
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+// app.use("/api", routerApi);
 
 app.use(logger(formatsLogger));
 app.use(cors());
